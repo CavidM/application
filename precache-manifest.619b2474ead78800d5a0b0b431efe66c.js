a0df24901d6c86a6385b72b34117225e30fa9d1f
CavidM <cavid2409@gmail.com>
2020-09-22 17:19:04 +0400

    Updates

---
+++ precache-manifest.619b2474ead78800d5a0b0b431efe66c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1592f4f5e36fe0be931d247e6f77639",
+    "url": "/application/index.html"
+  },
+  {
+    "revision": "a5ac084e1ba45b24513c",
+    "url": "/application/static/css/main.efb892c4.chunk.css"
+  },
+  {
+    "revision": "1044a0fcc557e2506251",
+    "url": "/application/static/js/2.66ac5f95.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/application/static/js/2.66ac5f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ac084e1ba45b24513c",
+    "url": "/application/static/js/main.77a22fcf.chunk.js"
+  },
+  {
+    "revision": "4a105f72b5c24c818baf",
+    "url": "/application/static/js/runtime-main.61a036ef.js"
+  }
+]);
